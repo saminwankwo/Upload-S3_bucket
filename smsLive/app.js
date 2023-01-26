30dc9e88f88c50209a33b84ed4a0a3f1a7914402
Nwankwo  Samuel <34462836+saminwankwo@users.noreply.github.com>
2023-01-26 12:52:44 +0100

    Update app.js

--- smsLive/app.js
+++ smsLive/app.js
@@ -1,4 +1,4 @@
-const api = "MA-b1342080-54b2-479b-a448-a9ca5d71dff3"
+
 // const sdk = require('api')('@smslive247api/v1.0#2vmizjp3blaut3hvq');
 
 // sdk.sendSmsMessage(
@@ -9,7 +9,7 @@ const api = "MA-b1342080-54b2-479b-a448-a9ca5d71dff3"
 
 const sdk = require('api')('@smslive247api/v1.0#2vmizjp3blaut3hvq');
 
-sdk.auth('MA-722eb3ba-6ef8-4878-89e2-c47fe6d08e2f');
+
 sdk.sendSmsMessage('{"senderID":"Credib","messageText":"Your verification code is 98948","mobileNumber":"07085515609","route":"none"}', {accept: 'application/json'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
